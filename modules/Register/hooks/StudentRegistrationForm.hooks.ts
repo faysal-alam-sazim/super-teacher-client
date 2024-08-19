20d3e85a231450673cbd533a89b442c3f100e8be
Faysal Alam <faysal.alam@sazim.io>
2024-08-19 14:40:14 +0600

    feat(ST-26): build student registration form

---
+++ modules/Register/hooks/StudentRegistrationForm.hooks.ts
@@ -0,0 +1,8 @@
+import { TStudentRegistrationFormData } from "../components/StudentRegistrationForm/StudentRegistrationForm.types";
+
+const onSubmit = (data: TStudentRegistrationFormData) => {
+  // TO DO: Remove this console log when add API call
+  console.log(data);
+};
+
+export { onSubmit };
