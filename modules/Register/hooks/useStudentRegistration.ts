70d9c5c855b7afa1e55bda9f60b9184fbbaf04e2
faysal-alam-sazim <faysal.alam@sazim.io>
2024-08-26 15:37:46 +0600

    Integrate Student Registration API (#4)
    * feat(ST-26): build student registration form
    
    * feat(ST-29): integrate student registration api with client

--- modules/Register/hooks/useStudentRegistration.ts
+++ modules/Register/hooks/useStudentRegistration.ts
@@ -1,9 +1,54 @@
+import { showNotification } from "@mantine/notifications";
+
+import { NOTIFICATION_AUTO_CLOSE_TIMEOUT_IN_MILLISECONDS } from "@/shared/constants/app.constants";
+import { useRegisterStudentMutation } from "@/shared/redux/rtk-apis/users/users.api";
+import { ERole, ICreateStudentDto } from "@/shared/typedefs";
+import { parseApiErrorMessage } from "@/shared/utils/errors";
+
 import { TStudentRegistrationFormData } from "../components/StudentRegistrationForm/StudentRegistrationForm.types";
 
 const useStudentRegistration = () => {
-  const onSubmit = (data: TStudentRegistrationFormData) => {
-    // TO DO: remove when add api call
-    console.log(data);
+  const [registerStudent] = useRegisterStudentMutation();
+
+  const onSubmit = async (data: TStudentRegistrationFormData) => {
+    const newStudent: ICreateStudentDto = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      gender: data.gender,
+      role: ERole.STUDENT,
+      email: data.email,
+      password: data.confirmPassword,
+      studentInput: {
+        phoneNumber: data.phoneNumber,
+        address: data.address,
+        educationLevel: data.educationLevel,
+        medium: data.medium,
+        class: data.class,
+        degree: data.degree,
+        degreeName: data.degreeName,
+        semesterYear: data.semesterYear,
+      },
+    };
+
+    try {
+      const res = await registerStudent(newStudent).unwrap();
+      if (res) {
+        showNotification({
+          title: "Success",
+          message: "Created Successfully",
+          autoClose: NOTIFICATION_AUTO_CLOSE_TIMEOUT_IN_MILLISECONDS,
+          color: "green",
+        });
+      }
+    } catch (error) {
+      const errorMessage = parseApiErrorMessage(error);
+      showNotification({
+        title: "Error",
+        message: errorMessage,
+        autoClose: NOTIFICATION_AUTO_CLOSE_TIMEOUT_IN_MILLISECONDS,
+        color: "red",
+      });
+    }
   };
 
   return { onSubmit };
